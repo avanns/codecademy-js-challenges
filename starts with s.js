6df8c01c11f4a9de5aaa202dec2c4d5ff8ac3e6f
a.vanns <aubinvanns@gmail.com>
2020-12-20 10:00:37 +0000

    added starts with s function

---
+++ starts with s.js
@@ -0,0 +1,6 @@
+let seaCreatures = [ "shark", "whale", "squid", "starfish", "narwhal" ];
+
+// Filter all creatures that start with "s" into a new list
+let filteredList = seaCreatures.filter(creature => creature[0] === "s");
+
+console.log(filteredList);
